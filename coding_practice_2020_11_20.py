6e991774e7a976e95c024261a2dee6fd8498effb
Marrow2312 <74619249+Marrow2312@users.noreply.github.com>
2020-11-20 13:29:07 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -9,3 +9,9 @@ print("Cans:", cans, "Packs:", packs,)
 print(packs * cans)
 
 print (3)
+length = input("What is the length of the prism in cm?")
+wdith = input("What is the width of the prism in cm?")
+height = input("What is the height of the prism in cm?")
+
+volume = height*width*length
+print("The volume is", volume, "cm"
