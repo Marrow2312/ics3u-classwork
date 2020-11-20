e507bfd7b91980d76d7fb7b28f5684bf8e1b5c4b
Marrow2312 <74619249+Marrow2312@users.noreply.github.com>
2020-11-20 13:26:03 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,3 +1,11 @@
 print (1)
 color = input ("What's your favourite color? : ")
 print ( color, "Thats inasane thats mine too!")
+
+print (2)
+cans = input ("How many cans do you have? : ")
+packs = input ("How many packs do you have? : ")
+print("Cans:", cans, "Packs:", packs,)
+print(packs * cans)
+
+print (3)
