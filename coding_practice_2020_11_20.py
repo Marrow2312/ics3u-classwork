87175d0d91c348dfb4f8295db34a8f0b51bf1c42
Marrow2312 <74619249+Marrow2312@users.noreply.github.com>
2020-11-20 13:52:46 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -3,15 +3,15 @@ color = input ("What's your favourite color? : ")
 print ( color, "Thats inasane thats mine too!")
 
 print (2)
-cans = input ("How many cans do you have? : ")
-packs = input ("How many packs do you have? : ")
+cans = float (input ("How many cans do you have? : "))
+packs = float (input ("How many packs do you have? : "))
 print("Cans:", cans, "Packs:", packs,)
 print(packs*cans)
 
 print (3)
-length = input("What is the length of the prism in cm?")
-wdith = input("What is the width of the prism in cm?")
-height = input("What is the height of the prism in cm?")
+length = float (input("What is the length of the prism in cm?"))
+width = float (input("What is the width of the prism in cm?"))
+height = float (input("What is the height of the prism in cm?"))
 
-volume = height*width*length
+volume = (height*width*length)
 print("The volume is", volume, "cm")
